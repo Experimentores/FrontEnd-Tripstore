7a2ad0d1fb10b33ecfff28bf5a944fef34a42301
DelfyNoLimits <nickhardav@gmail.com>
2023-09-24 10:54:41 -0500

    Upload Files

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'https://back-end-tripstore-production.up.railway.app/api/tripstore/v1',
+  apiUrl: 'https://apitripstore.onrender.com/api/tripstore/v1',
   localURL: 'http://localhost:8080/api/v1',
 };
