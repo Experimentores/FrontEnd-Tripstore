ef58eeb23c946e120a2c772fd3781d506b0653ac
DelfyNoLimits <nickhardav@gmail.com>
2023-09-24 23:20:54 -0500

    22:40

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'https://tripstoreapi.onrender.com/api/tripstore/v1',
+  apiUrl: 'http://localhost:8080/api/tripstore/v1',
   localURL: 'http://localhost:8080/api/v1',
 };
